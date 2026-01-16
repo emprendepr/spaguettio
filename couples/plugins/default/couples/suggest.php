f406e3ae2ddc31c82004023bb49d21fb52e60d9c
emprendepr <emprendepr7@gmail.com>
2026-01-15 22:01:32 -0400

    Delete couples directory

--- couples/plugins/default/couples/suggest.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Vista vacía de sugerencias de parejas.
- * Actualmente NO se usa porque el componente funciona con <datalist>.
- * Se deja solo para evitar errores si alguna vista vieja lo llama.
- */
